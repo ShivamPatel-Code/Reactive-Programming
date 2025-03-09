b3cc65948d2668a05ccc0623c538fadedeca6161
Shivam Patel <shivampatelcodes01@gmail.com>
2025-03-08 21:59:01 -0500

    Create employee model and repository

--- src/main/java/com/webflux/reactive_programming/model/Employee.java
+++ src/main/java/com/webflux/reactive_programming/model/Employee.java
@@ -1,4 +1,16 @@
 package com.webflux.reactive_programming.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
+    private Long id;
+    private String name;
+    private String role;
 }
--- src/main/java/com/webflux/reactive_programming/repository/EmployeeRepository.java
+++ src/main/java/com/webflux/reactive_programming/repository/EmployeeRepository.java
@@ -1,4 +1,34 @@
 package com.webflux.reactive_programming.repository;
 
+import com.webflux.reactive_programming.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Repository
 public class EmployeeRepository {
+    private final List<Employee> employeeList = new CopyOnWriteArrayList<>();
+
+    public EmployeeRepository(){
+        employeeList.add(new Employee(1L, "Shivam Patel", "Software Engineer"));
+        employeeList.add(new Employee(2L, "Ryan Cohen", "Architect"));
+    }
+
+    public List<Employee> findAll(){
+        return employeeList;
+    }
+
+    public Optional<Employee> findEmployeeById(Long id){
+        return employeeList.stream()
+                .filter(emp -> emp.getId().equals(id))
+                .findFirst();
+    }
+
+    public Employee saveEmployee(Employee employee){
+        employeeList.add(employee);
+        return employee;
+    }
 }
