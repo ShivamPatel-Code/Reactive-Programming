94bc9dd1f6e8e4cb3f3b3c0842e1994b4b34e219
Shivam Patel <shivampatelcodes01@gmail.com>
2025-03-08 23:23:37 -0500

    Add router and handler

--- src/main/java/com/webflux/reactive_programming/config/RouterConfig.java
+++ src/main/java/com/webflux/reactive_programming/config/RouterConfig.java
@@ -1,4 +1,22 @@
 package com.webflux.reactive_programming.config;
 
+import com.webflux.reactive_programming.handler.EmployeeHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class RouterConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> employeeRoutes(EmployeeHandler handler){
+        return route(GET("/employees"), handler::getAllEmployee)
+                .andRoute(GET("/employee/{id}"), handler::getEmployeeById)
+                .andRoute(POST("/employees"), handler::createEmployee);
+    }
 }
--- src/main/java/com/webflux/reactive_programming/handler/EmployeeHandler.java
+++ src/main/java/com/webflux/reactive_programming/handler/EmployeeHandler.java
@@ -1,4 +1,48 @@
 package com.webflux.reactive_programming.handler;
 
+import com.webflux.reactive_programming.model.Employee;
+import com.webflux.reactive_programming.service.EmployeeService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
 public class EmployeeHandler {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeHandler(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    public Mono<ServerResponse> getAllEmployee(ServerRequest request){
+        Flux<Employee> employeeFlux = Flux.fromIterable(employeeService.getAllEmployees());
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(employeeFlux, Employee.class);
+    }
+
+    public Mono<ServerResponse> getEmployeeById(ServerRequest request){
+        Long id = Long.valueOf(request.pathVariable("id"));
+        Mono<Employee> employeeMono = Mono.justOrEmpty(employeeService.getEmployeeById(id));
+        return employeeMono.flatMap(employee ->
+                ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(employee)
+                )
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> createEmployee(ServerRequest request){
+        Mono<Employee> employeeMono = request.bodyToMono(Employee.class);
+        return employeeMono.flatMap(employee ->
+                    ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .bodyValue(employeeService.createEmployee(employee))
+                );
+    }
+
 }
